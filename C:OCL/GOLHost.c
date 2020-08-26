cd9a6d8e829f7bf9d66f17dfdf8061376541db16
kipawaa <46198128+kipawaa@users.noreply.github.com>
2020-08-26 17:30:01 -0400

    Finalized computation
    Computation is now finalized. Graphics are rudimentary, but further updates should complete this

--- C:OCL/GOLHost.c
+++ C:OCL/GOLHost.c
@@ -7,6 +7,16 @@
 	#include<CL/cl.h>
 #endif
 
+// used to print the map (remove once a better display solution has been found)
+void printArr(int* map, int width, int height) {
+	for (int i = 0; i < width * height; i++) {
+		if (map[i]) printf("[]");
+		else printf("  ");
+		if (!(i % width)) printf("\n");
+	}
+	printf("\n");
+}
+
 int main() {
 	int width;
 	int height;
@@ -16,8 +26,14 @@ int main() {
 	printf("input the desired map height: ");
 	scanf("%d", &height);
 
-	// create the map
-	int** cellMap = (int**) malloc(width * height * sizeof(int));
+	// allocate memory for the map (OpenCL kernels don't seem to be able to handle 2D arrays, so this is one dimensional)
+	int* cellMap = (int*) malloc(width * height * sizeof(int));
+
+	// randomize the values in the map in range (0, 1)
+	for (int i = 0; i < width * height; i++) {
+		cellMap[i] = rand() % 2; // rand returns large integers, adding "% 2" envenly distributes 1s and 0s
+	}
+	printArr(cellMap, width, height);
 
 	// maximum size of the OCL file to be used
 	const size_t clFileSize = 0x100000;
@@ -42,50 +58,116 @@ int main() {
 
 	// automatically find and store an available platform in the platform variable
 	ret = clGetPlatformIDs(1, &platform, &numPlatforms);
-	
+	if (ret) {
+		printf("platform: %d\n", ret);
+		return ret;
+	}
+
 	// automatically find and store an available device that runs on the current platform in the device variable
 	ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, &numDevices);
+	if(ret) {
+		printf("device: %d\n", ret);
+		return ret;
+	}
 
 	// create a context
 	cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
+	if (ret) {
+		printf("context: %d\n", ret);
+		return ret;
+	}
 
 	// create command queue
 	cl_command_queue queue = clCreateCommandQueue(context, device, 0, &ret);
+	if (ret) {
+		printf("queue: %d\n", ret);
+		return ret;
+	}
 
 	// create buffers for data that will be passed to the compute device
 	cl_mem width_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
 	cl_mem height_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
 	cl_mem map_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, width * height * sizeof(int), NULL, &ret);
-	
-	// write the data to the buffers
-	ret = clEnqueueWriteBuffer(queue, width_obj, CL_TRUE, 0, sizeof(int), &width, 0, NULL, NULL);
-	ret = clEnqueueWriteBuffer(queue, height_obj, CL_TRUE, 0, sizeof(int), &height, 0, NULL, NULL);
-	ret = clEnqueueWriteBuffer(queue, map_obj, CL_TRUE, 0, width * height * sizeof(int), cellMap, 0, NULL, NULL);
+	if (ret) {
+		printf("buffers: %d\n", ret);
+		return ret;
+	}
 
 	// get the program from the string it's stored in
 	cl_program program = clCreateProgramWithSource(context, 1, (const char**)&clSource, &clFileSize, &ret);
+	if (ret) {
+		printf("program: %d\n", ret);
+		return ret;
+	}
+
+	// now finished with the kernel source code, release the memory associated with it
+	free(clSource);
 
 	// compile the program
 	ret = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+	if (ret) {
+		printf("build: %d\n", ret);
+		return ret;
+	}
+	// print build errors if they occurred (this is larger than other error catches since it prints the compiling errors for the kernel)
+	if (ret) {
+		size_t logSize;
+		clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &logSize);
+
+		char* log = (char*)malloc(logSize);
+		clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, log, NULL);
+		printf("%s\n", log);
+	}
 
 	// create the kernel
 	cl_kernel kernel = clCreateKernel(program, "computeLifeValues", &ret);
+	if (ret) {
+		printf("kernel: %d\n", ret);
+		return ret;
+	}
 
-	// set kernel arguments
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&map_obj);
-	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&width_obj);
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&height_obj);
+	// determine how many generations the user would like to see
+	printf("enter the number of generations you'd like to observe (recommended 1000): ");
+	int numGenerations = 0;
+	scanf("%d", &numGenerations);
+	
 
-	// declare work group and unit sizes
-	const size_t globalSize = width * height;
-	const size_t localSize = 1;
 
-	// execute the kernel
-	ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+	// this loop runs the simulation for numGenerations generations
+	for (int i = 0; i < numGenerations; i++) {
+		// write the data to the buffers
+		ret = clEnqueueWriteBuffer(queue, width_obj, CL_TRUE, 0, sizeof(int), &width, 0, NULL, NULL);
+		ret = clEnqueueWriteBuffer(queue, height_obj, CL_TRUE, 0, sizeof(int), &height, 0, NULL, NULL);
+		ret = clEnqueueWriteBuffer(queue, map_obj, CL_TRUE, 0, width * height * sizeof(int), cellMap, 0, NULL, NULL);
 
-	// read data from buffer
-	ret = clEnqueueReadBuffer(queue, map_obj, CL_TRUE, 0, width * height * sizeof(int), cellMap, 0, NULL, NULL);
+		// set kernel arguments
+		ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&map_obj);
+		ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&width_obj);
+		ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&height_obj);
+
+		// declare work group and unit sizes
+		const size_t globalSize = width * height;
+		const size_t localSize = 1;
+
+		// execute the kernel
+		ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+
+		// read data from buffer
+		ret = clEnqueueReadBuffer(queue, map_obj, CL_TRUE, 0, width * height * sizeof(int), cellMap, 0, NULL, NULL);
+
+		printArr(cellMap, width, height);
+	}
 
 	// clean up everything associated with OCL
+	clFinish(queue);
+	clFlush(queue);
+	clReleaseKernel(kernel);
+	clReleaseProgram(program);
+	clReleaseMemObject(map_obj);
+	clReleaseMemObject(width_obj);
+	clReleaseMemObject(height_obj);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
 
+	free(cellMap);
 }
--- C:OCL/GOLKernel.cl
+++ C:OCL/GOLKernel.cl
@@ -1,12 +1,12 @@
 // much of this code is branchless. If you're confused, check out https://www.youtube.com/watch?v=bVJ-mWWL7cE&list=WL&index=15&t=676s or my examples @ https://github.com/kipawaa/Little-Projects/blob/master/C/branchless.c
-__kernel void computeLifeValues(__global int** cellMap, __global int height, __global int width) {
+__kernel void computeLifeValues(__global int* cellMap, __global int* height, __global int* width) {
 	// cellMap is a 2d array with 0 for a dead cell and 1 for a live cell, width and height are the dimensions of cellMap
 	
 	// get thread index which will be used to calculate a cell at the same index
 	int idx = get_global_id(0);
 
 	// set the number of living cells around it to default at 0
-	surroundingLifeCount = 0;
+	int surroundingLifeCount = 0;
 
 	// (*(cellMap + idx) % width == 0)		true if the COI (cell of interest) is at the left edge of the map
 	// (*(cellMap + idx) % width == width - 1)	true if the COI is at the right edge of the map
@@ -14,15 +14,33 @@ __kernel void computeLifeValues(__global int** cellMap, __global int height, __g
 	// (idx / width == height)			true if the COI is at the bottom edge of the map
 	
 	// determine which cells around it are applicable and alive
-	surroundingLifeCount += !(*(cellMap + idx) % width == 0) * (*(cellMap + idx - 1)); // counts the value to the left of the COI if the COI is not against the left wall
-	surroundingLifecount += !(*(cellMap + idx) % width == width - 1) * (*(cellMap + idx + 1)); // counts value to the right if the COI is not against the right wall
-	surroundingLifeCount += !(*(cellMap + idx) % width == 0) * !(idx / width == 0) * (*(cellMap + idx - width - 1)); // up and left
-	surroundingLifeCount += !(idx / width == 0) * (*(cellMap + idx - width)); // up
-	surroundingLifeCount += !(*(cellMap + idx) % width == width - 1) * !(idx / width == 0) * (*(cellMap + idx - width + 1)); // up and right
-	surroundingLifeCount += !(*(cellMap + idx) % width == 0) * !(idx / width == height) * (*(cellMap + idx + width - 1)); // down and left
-	surroundingLifecount += !(idx / width == height) * (*(cellMap + idx + width)); // down
-	surroundingLifeCount += !(*(cellMap + idx % width == width - 1) * !(idx / width == height) * (*(cellMap + idx + width + 1)); // down and right
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == 0) * // conditional for left side (ensures there is a cell to the left)
+				(*(cellMap + idx - 1)); // value of the cell to the left
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == *(width) - 1) * // conditional for the right side
+				(*(cellMap + idx + 1)); // value of the cell to the right
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == 0) * // conditional for the left
+				!(idx / *(width) == 0) * // conditional for the cell above
+				(*(cellMap + idx - *(width) - 1)); // value of the cell above and to the left
+	surroundingLifeCount += !(idx / *(width) == 0) * // conditional for above
+				(*(cellMap + idx - *(width))); // value of the cell above
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == *(width) - 1) * // conditional for the right
+				!(idx / *(width) == 0) * // conditional for above
+				(*(cellMap + idx - *(width) + 1)); // value of the cell above and to the right
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == 0) * // conditional for the left
+				!(idx / *(width) == *(height)) * // conditional for below
+				(*(cellMap + idx + *(width) - 1)); // value of the cell below and to the left
+	surroundingLifeCount += !(idx / *(width) == *(height)) * // conditional for below
+				(*(cellMap + idx + *(width))); // value of the cell below
+	surroundingLifeCount += !(*(cellMap + idx) % *(width) == *(width) - 1) * // conditional for the right
+				!(idx / *(width) == *(height)) * // conditional for below
+				(*(cellMap + idx + *(width) + 1)); // value of the cell below and to the right
 	
 	// set the value of the cell based on the default rules of Conway's GOL 
-	*(cellMap + idx) = 0 * (surroundingLifeCount < 3) + (surroundingLifeCount == 3) + 0 * (surroundingLifeCount > 4);
+	if (*(cellMap + idx) && (surroundingLifeCount == 2 || surroundingLifeCount == 3)) {
+		*(cellMap + idx) = 1;
+	} else if (!*(cellMap + idx) && surroundingLifeCount == 3) {
+		*(cellMap + idx) = 1;
+	} else {
+		*(cellMap + idx) = 0;
+	}
 }
